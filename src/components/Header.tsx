1734509c5fc592b14c8dde2569a3b1baec1ec929
Neda-Veleva <neda.v.veleva@gmail.com>
2025-11-21 09:50:38 +0200

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,10 +19,23 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-6">
         <div className="flex items-center justify-center h-20 relative">
           <div className="absolute left-0 flex items-center gap-3">
-       
-            <div>
-              <h1 className="font-serif text-6xl text-gold-600 font-bold">PY</h1>
-            </div>
+            <svg width="60" height="33.4" viewBox="0 0 100 55.67" className="h-10 w-auto">
+              <defs>
+                <linearGradient id="logoGradient" x1="0" y1="1" x2="1" y2="0">
+                  <stop offset="0" stopColor="#fff7ae"></stop>
+                  <stop offset="0.21" stopColor="#9b6326"></stop>
+                  <stop offset="0.38" stopColor="#fff7ae"></stop>
+                  <stop offset="0.53" stopColor="#d3c175"></stop>
+                  <stop offset="0.65" stopColor="#fceec4"></stop>
+                  <stop offset="0.7" stopColor="#b78626"></stop>
+                  <stop offset="0.82" stopColor="#fffacf"></stop>
+                  <stop offset="1" stopColor="#9b6326"></stop>
+                </linearGradient>
+              </defs>
+              <g transform="matrix(3.731343389805563,0,0,3.731343389805563,-8.208955635496556,-18.95522235629017)" fill="url(#logoGradient)">
+                <path d="M7.22 11.46 l2.64 0 q2.2 0 2.2 -2.12 q0 -1.1 -0.55 -1.59 t-1.65 -0.49 l-5.48 0 l0 4.8 q0.62 -0.36 1.4 -0.5 q0.58 -0.1 1.44 -0.1 z M4.38 20 l-2.18 0 l0 -14.92 l7.66 0 q1.32 0 2.3 0.51 t1.52 1.47 t0.54 2.27 t-0.54 2.29 t-1.53 1.51 t-2.29 0.53 l-2.64 0 q-1.72 0 -2.84 0.6 l0 5.74 z M22.36 11.64 q1.34 -1.4 2.42 -3.12 t1.72 -3.44 l2.5 0 q-1.02 2.38 -2.48 4.56 q-1.32 1.98 -3.08 3.9 l0 6.46 l-2.16 0 l0 -6.46 q-1.76 -1.92 -3.08 -3.9 q-1.46 -2.18 -2.48 -4.56 l2.5 0 q0.64 1.72 1.72 3.44 t2.42 3.12 z"></path>
+              </g>
+            </svg>
           </div>
 
           <nav className="hidden lg:flex items-center gap-8">
